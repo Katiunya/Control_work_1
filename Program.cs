68a1da385713d7389a95b8ddbd65119508d27f7f
Ekaterina Pabauskaya <Katiunya0103@gmail.com>
2023-03-12 14:12:53 +0300

    the programm added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Input the number of array elements: ");
+int size  = Convert.ToInt32(Console.ReadLine());
+
+
+string[] array = new string[size];
+
+for (int i = 0; i < size; i++){
+	Console.Write("Input an array element: ");
+    string result = Convert.ToString(Console.ReadLine());
+
+	array[i] = result;  
+}
+
+Console.WriteLine();
+Console.Write("The entered array: [");
+Console.Write(string.Join(",", array));
+Console.Write("]");
+
+int count = 0;
+int maxChar = 3;
+
+for (int i = 0; i < array.Length; i++){
+	if(array[i].Length <= maxChar){
+		count++;
+	}   
+}
+
+Console.WriteLine();
+Console.Write("The resulting array: [");
+
+string[] newArray = new string[count];
+int j = 0;
+
+for (int i = 0; i < array.Length; i++){
+    if(array[i].Length <= maxChar){
+        newArray[j] = array[i];
+        Console.Write(newArray[j] + ",");
+        j++;
+    }
+}
+
+Console.Write("]");
+
